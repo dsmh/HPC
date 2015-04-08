1f31c36ca27d6b03b2219f7cc440ef561ef7e5f3
utp <utp@utp>
2015-04-08 11:41:32 -0500

    Correccion para datos grandes con multiples llamados al kernel

--- Sumatoria de vectores/sumavectores.cu
+++ Sumatoria de vectores/sumavectores.cu
@@ -6,21 +6,25 @@
 #define BLOCK_SIZE 512
 #define ELEMENTS 512
 
-__global__ void sum(int *A, int *C) {
-  __shared__ int temp[ELEMENTS];
-  int idx = threadIdx.x + blockDim.x * blockIdx.x;
-  int local_idx = threadIdx.x;
-  temp[local_idx] = A[idx];
-  int i = blockDim.x;
-  __syncthreads();
-  while (i != 0) {
-    if (idx + i < ELEMENTS && local_idx < i)
-      temp[local_idx] += temp[local_idx + i];
-    i /= 2;
+__global__ void sum(int *g_idata, int *g_odata,int num_vec) {
+	__shared__ int sdata[BLOCK_SIZE];
+    // each thread loads one element from global to shared mem
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+  	if(i<num_vec)
+    	sdata[tid] = g_idata[i];
+ 	else
+      sdata[tid]=0;
     __syncthreads();
-  }
-  if (local_idx == 0)
-    C[blockIdx.x] = temp[0];
+    // do reduction in shared mem
+    for(unsigned int s=blockDim.x/2; s > 0; s >>= 1) {
+        if(tid < s){
+            sdata[tid] += sdata[tid + s];
+        }
+        __syncthreads();
+    }
+    // write result for this block to global mem
+    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
 }
 
 void sumatoriaSec(int *A, int *r) {
@@ -53,28 +57,36 @@ int main() {
 
   int *d_A;
   int *d_R;
-  clock_t start2 = clock();
+  
   cudaMalloc(&d_A, bytes);
   cudaMalloc(&d_R, bytes);
-
+  clock_t start2 = clock();
   cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice);
   cudaMemcpy(d_R, R, bytes, cudaMemcpyHostToDevice);
 
   // Number of threads in each thread block
   // bloques
   float blocksize = BLOCK_SIZE;
-  dim3 dimGrid(ceil(ELEMENTS / blocksize), 1, 1);
-  // hilos
-  dim3 dimBlock(blocksize, 1, 1);
-
-  //	sumatoria<<<dimGrid,dimBlock>>>(d_A,d_R);
-  sum << <dimGrid, dimBlock>>> (d_A, d_R);
-  // Copy array back to host
+  int i = ELEMENTS;
+  while(i>1){
+            int grid=ceil(i/blocksize);
+          dim3 dimGrid(grid,1,1);
+          // hilos
+          dim3 dimBlock(blocksize, 1, 1);
+          //	sumatoria<<<dimGrid,dimBlock>>>(d_A,d_R);
+          sum<<<dimGrid, dimBlock>>>(d_A, d_R,i);
+          cudaDeviceSynchronize();
+          // Copy array back to host
+          cudaMemcpy(d_A, d_R, bytes, cudaMemcpyDeviceToDevice);
+          i=ceil(i/blocksize);
+  }
   cudaMemcpy(R, d_R, bytes, cudaMemcpyDeviceToHost);
   clock_t end2 = clock();
   double elapsed_seconds2 = end2 - start2;
-  printf("Tiempo algoritmo paralelo: %lf\n",
-         (elapsed_seconds2 / CLOCKS_PER_SEC));
+  printf("Tiempo algoritmo paralelo: %lf\n", (elapsed_seconds2 / CLOCKS_PER_SEC));
+  
+  
+  
 
   if (s != R[0])
     printf(
